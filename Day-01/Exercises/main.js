155ebf120f3a1f87a63b188eebd1c95f8dd2b65e
vegadevh <vegadevh@gmail.com>
2024-11-28 15:05:36 -0600

    Adding Day 01 to 30 Days of JavaScript Challenge

---
+++ Day-01/Exercises/main.js
@@ -0,0 +1,65 @@
+// Write a single line comment which says, comments can make code readable
+
+// Comments can make code readable
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+// Welcome to 30DaysOfJavaScript
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+/**
+ * Comments can make code readable, easy to reuse and informative
+ */
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let var_string = '';
+let var_bool = true;
+let var_undefined = undefined;
+let var_null = null;
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+const checkTypes = (variable) => {
+    console.log(typeof (variable));
+}
+
+checkTypes(var_string);
+checkTypes(var_bool);
+checkTypes(var_undefined);
+checkTypes(var_null);
+
+// Declare four variables without assigning values
+
+let var1;
+let var2;
+let var3;
+let var4;
+
+// Declare four variables with assigned values
+
+let var_1 = 1;
+let var_2 = 1;
+let var_3 = 1;
+let var_4 = 1;
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let name = 'Diego';
+let lastname = 'Vega';
+let marital_status = 'Single';
+let country = 'El Salvador';
+let age = 27;
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let name_EX = 'Diego', lastname_EX = 'Vega', marital_status_EX = 'Single', country_EX = 'El Salvador', age_EX = 27;
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 27;
+let yourAge = 30;
+
+console.log(myAge);
+console.log(yourAge);
