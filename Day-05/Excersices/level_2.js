9cda5e2a6dc34832bd5f650cc0bb1c34a13d2bac
Diego Vega <vegadevh@gmail.com>
2024-12-25 00:05:15 -0800

    Adding excercises level 3 of day 5 managing arrays

--- Day-05/Excersices/level_2.js
+++ Day-05/Excersices/level_2.js
@@ -56,11 +56,21 @@ checkShoppingCart();
 
 console.log('3. - ', shoppingCart, checkedCart);
 
-
 // In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
 
+if (countries.includes('Ethiopia')) {
+    console.log('4. - ', countries[countries.indexOf('Ethiopia')].toUpperCase());
+} else {
+    countries.push('Ethiopia')
+    console.log('4. - ', countries);
+}
 // In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
-
+if (webTechs.includes('Sass')) {
+    console.log('5. - ', 'Sass is a CSS preprocess');
+} else {
+    webTechs.push('Sass')
+    console.log('5. - ', webTechs);
+}
 // Concatenate the following two variables and store it in a fullStack variable.
 /* 
 const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
@@ -70,4 +80,9 @@ console.log(fullStack)
 
 ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
 */
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+const backEnd = ['Node', 'Express', 'MongoDB'];
+
+const fullStack = [...frontEnd, ...backEnd];
+console.log('6. - ', fullStack);
 
