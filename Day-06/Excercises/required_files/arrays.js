32728b2a77089a7a1b02236b6af8c883584b7a46
Diego Vega <vegadevh@gmail.com>
2024-12-31 18:59:26 -0600

    Adding additional file to import arrays

---
+++ Day-06/Excercises/required_files/arrays.js
@@ -0,0 +1,25 @@
+export const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+];
+
+export const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+];
+
+export const mernStack = ['MongoDB', 'Express', 'React', 'Node'];
